971ac3fc837d7519a70601ddb58b91a0fc344e1c
fjkk <fujikake.takayoshi@gmail.com>
2023-12-16 17:57:14 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    const [todoText, setTodoText] = useState("");
+    const [todoList, setNewTodoList] = React.useState([]);
+    
+    const onChangeTodoText = (event) => {
+        setTodoText(event.target.value);
+    }
+    
+    const onClickAdd = () => {
+        if (todoText === "") return;
+        const newTodo = {
+            comment: todoText,
+            status: "作業中"
+        }
+        
+        todoList.push(newTodo);
+        setTodoText("");
+    };
+    
+    return (
+        <>
+            <div className="task-area">
+                <h1>ToDoリスト</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>コメント</td>
+                            <td>状態</td>
+                        </tr>
+                    </thead>
+                    <tbody id="todo-body">
+                        {todoList.map((todo, index) => (
+                            <tr>
+                                <td>{index}</td>
+                                <td>{todo.comment}</td>
+                                <td><button>{todo.status}</button></td>
+                                <td><button>削除</button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            
+            <h2>新規タスクの追加</h2>
+            <div className="add-todo">
+                <input value={todoText} onChange={onChangeTodoText} />
+                <button onClick={onClickAdd}>追加</button>
+            </div>
+        </>
+    );
 }
-
-export default App;
