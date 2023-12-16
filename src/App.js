67cf22a11c2d5c6b6685435e48dabdc8f4052ed6
fjkk <fujikake.takayoshi@gmail.com>
2023-12-16 18:39:24 +0900

    filter added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 export const App = () => {
     const [todoText, setTodoText] = useState("");
     const [todoList, setNewTodoList] = React.useState([]);
+    const [filteredTodoList, setFilteredTodoList] = React.useState([]);
+    const [radio, setRadio] = React.useState('all');
     
     const onChangeTodoText = (event) => {
         setTodoText(event.target.value);
@@ -34,8 +36,37 @@ export const App = () => {
         setNewTodoList(switchTodoList);
     };
     
+    const handleChange = (event) => {
+        setRadio(event.target.value);
+        if (event.target.value === "imcomplete") {
+            const imcompleteTodoList = [...todoList].filter((todo) => todo.status === "作業中");
+            setFilteredTodoList(imcompleteTodoList);
+        } else if (event.target.value === "complete") {
+            const completeTodoList = [...todoList].filter((todo) => todo.status === "完了");
+            setFilteredTodoList(completeTodoList);
+        } return;
+    };
+    
+    
     return (
         <>
+            <div className="complete-area">
+                <label>
+                    <input type="radio" value="all" onChange={handleChange} checked={radio === 'all'} />
+                    すべて
+                </label>
+                
+                <label>
+                    <input type="radio" value="imcomplete" onChange={handleChange} checked={radio === 'imcomplete'} />
+                    作業中
+                </label>
+                
+                <label>
+                    <input type="radio" value="complete" onChange={handleChange} checked={radio === 'complete'} />
+                    完了
+                </label>
+            </div>
+        
             <div className="task-area">
                 <h1>ToDoリスト</h1>
                 <table>
@@ -46,6 +77,8 @@ export const App = () => {
                             <td>状態</td>
                         </tr>
                     </thead>
+                    {
+                        radio === 'all' ? 
                     <tbody id="todo-body">
                         {todoList.map((todo, index) => (
                             <tr>
@@ -56,6 +89,18 @@ export const App = () => {
                             </tr>
                         ))}
                     </tbody>
+                    :
+                    <tbody id="todo-body">
+                        {filteredTodoList.map((todo, index) => (
+                            <tr>
+                                <td>{index}</td>
+                                <td>{todo.comment}</td>
+                                <td><button onClick={() => onClickSwitch(index)}>{todo.status}</button></td>
+                                <td><button onClick={() => onClickDelete(index)}>削除</button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    }
                 </table>
             </div>
             
